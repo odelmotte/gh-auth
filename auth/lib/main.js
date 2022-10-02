bff7c034bda6c682145384bef7ad13efc7560f18
Olivier Delmotte <hangar18rip@users.noreply.github.com>
2022-10-02 23:13:14 +0200

    node handling

--- auth/lib/main.js
+++ auth/lib/main.js
@@ -14,7 +14,7 @@ var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (
 var __importStar = (this && this.__importStar) || function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
     __setModuleDefault(result, mod);
     return result;
 };
@@ -30,9 +30,10 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 const core = __importStar(require("@actions/core"));
 const exec = __importStar(require("@actions/exec"));
-const io = __importStar(require("@actions/io"));
-const actions_secret_parser_1 = require("actions-secret-parser");
-const ServicePrincipalLogin_1 = require("./PowerShell/ServicePrincipalLogin");
+// import { ExecOptions } from '@actions/exec/lib/interfaces';
+// import * as io from '@actions/io';
+// import { FormatType, SecretParser } from 'actions-secret-parser';
+// import { ServicePrincipalLogin } from './PowerShell/ServicePrincipalLogin';
 var azPath;
 var prefix = !!process.env.AZURE_HTTP_USER_AGENT ? `${process.env.AZURE_HTTP_USER_AGENT}` : "";
 var azPSHostEnv = !!process.env.AZUREPS_HOST_ENVIRONMENT ? `${process.env.AZUREPS_HOST_ENVIRONMENT}` : "";
@@ -40,97 +41,96 @@ function main() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             //Options for error handling
-            const loginOptions = {
-                silent: true,
-                listeners: {
-                    stderr: (data) => {
-                        let error = data.toString();
-                        let startsWithWarning = error.toLowerCase().startsWith('warning');
-                        let startsWithError = error.toLowerCase().startsWith('error');
-                        // printing ERROR
-                        if (error && error.trim().length !== 0 && !startsWithWarning) {
-                            if (startsWithError) {
-                                //removing the keyword 'ERROR' to avoid duplicates while throwing error
-                                error = error.slice(5);
-                            }
-                            core.setFailed(error);
-                        }
-                    }
-                }
-            };
-            // Set user agent variable
+            // const loginOptions: ExecOptions = {
+            //     silent: true,
+            //     listeners: {
+            //         stderr: (data: Buffer) => {
+            //             let error = data.toString();
+            //             let startsWithWarning = error.toLowerCase().startsWith('warning');
+            //             let startsWithError = error.toLowerCase().startsWith('error');
+            //             // printing ERROR
+            //             if (error && error.trim().length !== 0 && !startsWithWarning) {
+            //                 if(startsWithError) {
+            //                     //removing the keyword 'ERROR' to avoid duplicates while throwing error
+            //                     error = error.slice(5);
+            //                 }
+            //                 core.setFailed(error);
+            //             }
+            //         }
+            //     }
+            // }
+            // // Set user agent variable
             var isAzCLISuccess = false;
-            let usrAgentRepo = `${process.env.GITHUB_REPOSITORY}`;
-            let actionName = 'AzureLogin';
-            let userAgentString = (!!prefix ? `${prefix}+` : '') + `GITHUBACTIONS/${actionName}@v1_${usrAgentRepo}`;
-            let azurePSHostEnv = (!!azPSHostEnv ? `${azPSHostEnv}+` : '') + `GITHUBACTIONS/${actionName}@v1_${usrAgentRepo}`;
-            core.exportVariable('AZURE_HTTP_USER_AGENT', userAgentString);
-            core.exportVariable('AZUREPS_HOST_ENVIRONMENT', azurePSHostEnv);
-            azPath = yield io.which("az", true);
-            core.debug(`az cli version used: ${azPath}`);
-            let azureSupportedCloudName = new Set([
-                "azureusgovernment",
-                "azurechinacloud",
-                "azuregermancloud",
-                "azurecloud",
-                "azurestack"
-            ]);
-            let output = "";
-            const execOptions = {
-                listeners: {
-                    stdout: (data) => {
-                        output += data.toString();
-                    }
-                }
-            };
-            yield executeAzCliCommand("--version", true, execOptions);
-            core.debug(`az cli version used:\n${output}`);
-            let creds = core.getInput('creds', { required: false });
-            let secrets = creds ? new actions_secret_parser_1.SecretParser(creds, actions_secret_parser_1.FormatType.JSON) : null;
+            // let usrAgentRepo = `${process.env.GITHUB_REPOSITORY}`;
+            // let actionName = 'AzureLogin';
+            // let userAgentString = (!!prefix ? `${prefix}+` : '') + `GITHUBACTIONS/${actionName}@v1_${usrAgentRepo}`;
+            // let azurePSHostEnv = (!!azPSHostEnv ? `${azPSHostEnv}+` : '') + `GITHUBACTIONS/${actionName}@v1_${usrAgentRepo}`;
+            // core.exportVariable('AZURE_HTTP_USER_AGENT', userAgentString);
+            // core.exportVariable('AZUREPS_HOST_ENVIRONMENT', azurePSHostEnv);
+            // azPath = await io.which("az", true);
+            // core.debug(`az cli version used: ${azPath}`);
+            // let azureSupportedCloudName = new Set([
+            //     "azureusgovernment",
+            //     "azurechinacloud",
+            //     "azuregermancloud",
+            //     "azurecloud",
+            //     "azurestack"]);
+            // let output: string = "";
+            // const execOptions: any = {
+            //     listeners: {
+            //         stdout: (data: Buffer) => {
+            //             output += data.toString();
+            //         }
+            //     }
+            // };
+            // await executeAzCliCommand("--version", true, execOptions);
+            // core.debug(`az cli version used:\n${output}`);
+            // let creds = core.getInput('creds', { required: false });
+            // let secrets = creds ? new SecretParser(creds, FormatType.JSON) : null;
             let environment = core.getInput("environment").toLowerCase();
-            const enableAzPSSession = core.getInput('enable-AzPSSession').toLowerCase() === "true";
-            const allowNoSubscriptionsLogin = core.getInput('allow-no-subscriptions').toLowerCase() === "true";
-            //Check for the credentials in individual parameters in the workflow.
-            var servicePrincipalId = core.getInput('client-id', { required: false });
-            var servicePrincipalKey = null;
-            var tenantId = core.getInput('tenant-id', { required: false });
-            var subscriptionId = core.getInput('subscription-id', { required: false });
-            var resourceManagerEndpointUrl = "https://management.azure.com/";
-            var enableOIDC = true;
+            // const enableAzPSSession = core.getInput('enable-AzPSSession').toLowerCase() === "true";
+            // const allowNoSubscriptionsLogin = core.getInput('allow-no-subscriptions').toLowerCase() === "true";
+            // //Check for the credentials in individual parameters in the workflow.
+            // var servicePrincipalId = core.getInput('client-id', { required: false });
+            // var servicePrincipalKey = null;
+            // var tenantId = core.getInput('tenant-id', { required: false });
+            // var subscriptionId = core.getInput('subscription-id', { required: false });
+            // var resourceManagerEndpointUrl = "https://management.azure.com/";
+            // var enableOIDC = true;
             var federatedToken = null;
-            // If any of the individual credentials (clent_id, tenat_id, subscription_id) is present.
-            if (servicePrincipalId || tenantId || subscriptionId) {
-                //If few of the individual credentials (clent_id, tenat_id, subscription_id) are missing in action inputs.
-                if (!(servicePrincipalId && tenantId && (subscriptionId || allowNoSubscriptionsLogin)))
-                    throw new Error("Few credentials are missing. ClientId, tenantId are mandatory. SubscriptionId is also mandatory if allow-no-subscriptions is not set.");
-            }
-            else {
-                if (creds) {
-                    core.debug('using creds JSON...');
-                    enableOIDC = false;
-                    servicePrincipalId = secrets.getSecret("$.clientId", true);
-                    servicePrincipalKey = secrets.getSecret("$.clientSecret", true);
-                    tenantId = secrets.getSecret("$.tenantId", true);
-                    subscriptionId = secrets.getSecret("$.subscriptionId", true);
-                    resourceManagerEndpointUrl = secrets.getSecret("$.resourceManagerEndpointUrl", false);
-                }
-                else {
-                    throw new Error("Credentials are not passed for Login action.");
-                }
-            }
-            //generic checks 
-            //servicePrincipalKey is only required in non-oidc scenario.
-            if (!servicePrincipalId || !tenantId || !(servicePrincipalKey || enableOIDC)) {
-                throw new Error("Not all values are present in the credentials. Ensure clientId, clientSecret and tenantId are supplied.");
-            }
-            if (!subscriptionId && !allowNoSubscriptionsLogin) {
-                throw new Error("Not all values are present in the credentials. Ensure subscriptionId is supplied.");
-            }
-            if (!azureSupportedCloudName.has(environment)) {
-                throw new Error("Unsupported value for environment is passed.The list of supported values for environment are ‘azureusgovernment', ‘azurechinacloud’, ‘azuregermancloud’, ‘azurecloud’ or ’azurestack’");
-            }
+            // // If any of the individual credentials (clent_id, tenat_id, subscription_id) is present.
+            // if (servicePrincipalId || tenantId || subscriptionId) {
+            //     //If few of the individual credentials (clent_id, tenat_id, subscription_id) are missing in action inputs.
+            //     if (!(servicePrincipalId && tenantId && (subscriptionId || allowNoSubscriptionsLogin)))
+            //         throw new Error("Few credentials are missing. ClientId, tenantId are mandatory. SubscriptionId is also mandatory if allow-no-subscriptions is not set.");
+            // }
+            // else {
+            //     if (creds) {
+            //         core.debug('using creds JSON...');
+            //         enableOIDC = false;
+            //         servicePrincipalId = secrets.getSecret("$.clientId", true);
+            //         servicePrincipalKey = secrets.getSecret("$.clientSecret", true);
+            //         tenantId = secrets.getSecret("$.tenantId", true);
+            //         subscriptionId = secrets.getSecret("$.subscriptionId", true);
+            //         resourceManagerEndpointUrl = secrets.getSecret("$.resourceManagerEndpointUrl", false);
+            //     }
+            //     else {
+            //         throw new Error("Credentials are not passed for Login action.");
+            //     }
+            // }
+            // //generic checks 
+            // //servicePrincipalKey is only required in non-oidc scenario.
+            // if (!servicePrincipalId || !tenantId || !(servicePrincipalKey || enableOIDC)) {
+            //     throw new Error("Not all values are present in the credentials. Ensure clientId, clientSecret and tenantId are supplied.");
+            // }
+            // if (!subscriptionId && !allowNoSubscriptionsLogin) {
+            //     throw new Error("Not all values are present in the credentials. Ensure subscriptionId is supplied.");
+            // }
+            // if (!azureSupportedCloudName.has(environment)) {
+            //     throw new Error("Unsupported value for environment is passed.The list of supported values for environment are ‘azureusgovernment', ‘azurechinacloud’, ‘azuregermancloud’, ‘azurecloud’ or ’azurestack’");
+            // }
             // OIDC specific checks
-            if (enableOIDC) {
+            if (true) {
                 console.log('Using OIDC authentication...');
                 try {
                     //generating ID-token
@@ -141,18 +141,42 @@ function main() {
                             throw new Error(`Your current environment - "${environment}" is not supported for OIDC login.`);
                         let [issuer, subjectClaim] = yield jwtParser(federatedToken);
                         console.log("Federated token details: \n issuer - " + issuer + " \n subject claim - " + subjectClaim);
-                        
-                        core.exportVariable('federatedToken', federatedToken);
-                        core.exportVariable('issuer', issuer);
-                        core.exportVariable('subjectClaim', subjectClaim);
                     }
                 }
                 catch (error) {
                     core.error(`${error.message.split(':')[1]}. Please make sure to give write permissions to id-token in the workflow.`);
                 }
             }
-
-            // yield executeAzCliCommand(`cloud set -n "${environment}"`, false);
+            // Attempting Az cli login
+            // if (environment == "azurestack") {
+            //     if (!resourceManagerEndpointUrl) {
+            //         throw new Error("resourceManagerEndpointUrl is a required parameter when environment is defined.");
+            //     }
+            //     console.log(`Unregistering cloud: "${environment}" first if it exists`);
+            //     try {
+            //         await executeAzCliCommand(`cloud set -n AzureCloud`, true);
+            //         await executeAzCliCommand(`cloud unregister -n "${environment}"`, false);
+            //     }
+            //     catch (error) {
+            //         console.log(`Ignore cloud not registered error: "${error}"`);
+            //     }
+            //     console.log(`Registering cloud: "${environment}" with ARM endpoint: "${resourceManagerEndpointUrl}"`);
+            //     try {
+            //         let baseUri = resourceManagerEndpointUrl;
+            //         if (baseUri.endsWith('/')) {
+            //             baseUri = baseUri.substring(0, baseUri.length - 1); // need to remove trailing / from resourceManagerEndpointUrl to correctly derive suffixes below
+            //         }
+            //         let suffixKeyvault = ".vault" + baseUri.substring(baseUri.indexOf('.')); // keyvault suffix starts with .
+            //         let suffixStorage = baseUri.substring(baseUri.indexOf('.') + 1); // storage suffix starts without .
+            //         let profileVersion = "2019-03-01-hybrid";
+            //         await executeAzCliCommand(`cloud register -n "${environment}" --endpoint-resource-manager "${resourceManagerEndpointUrl}" --suffix-keyvault-dns "${suffixKeyvault}" --suffix-storage-endpoint "${suffixStorage}" --profile "${profileVersion}"`, false);
+            //     }
+            //     catch (error) {
+            //         core.error(`Error while trying to register cloud "${environment}": "${error}"`);
+            //     }
+            //     console.log(`Done registering cloud: "${environment}"`)
+            // }
+            // await executeAzCliCommand(`cloud set -n "${environment}"`, false);
             // console.log(`Done setting cloud: "${environment}"`);
             // // Attempting Az cli login
             // var commonArgs = ["--service-principal",
@@ -166,25 +190,33 @@ function main() {
             //     commonArgs = commonArgs.concat("--federated-token", federatedToken);
             // }
             // else {
-            //     console.log("Note: Azure/login action also supports OIDC login mechanism. Refer https://github.com/azure/login#configure-a-service-principal-with-a-federated-credential-to-use-oidc-based-authentication for more details.");
+            //     console.log("Note: Azure/login action also supports OIDC login mechanism. Refer https://github.com/azure/login#configure-a-service-principal-with-a-federated-credential-to-use-oidc-based-authentication for more details.")
             //     commonArgs = commonArgs.concat("-p", servicePrincipalKey);
             // }
-            // yield executeAzCliCommand(`login`, true, loginOptions, commonArgs);
+            // await executeAzCliCommand(`login`, true, loginOptions, commonArgs);
             // if (!allowNoSubscriptionsLogin) {
             //     var args = [
             //         "--subscription",
             //         subscriptionId
             //     ];
-            //     yield executeAzCliCommand(`account set`, true, loginOptions, args);
+            //     await executeAzCliCommand(`account set`, true, loginOptions, args);
             // }
             // isAzCLISuccess = true;
             // if (enableAzPSSession) {
             //     // Attempting Az PS login
             //     console.log(`Running Azure PS Login`);
-            //     var spnlogin;
-            //     spnlogin = new ServicePrincipalLogin_1.ServicePrincipalLogin(servicePrincipalId, servicePrincipalKey, federatedToken, tenantId, subscriptionId, allowNoSubscriptionsLogin, environment, resourceManagerEndpointUrl);
-            //     yield spnlogin.initialize();
-            //     yield spnlogin.login();
+            //     var spnlogin: ServicePrincipalLogin;
+            //     spnlogin = new ServicePrincipalLogin(
+            //         servicePrincipalId,
+            //         servicePrincipalKey,
+            //         federatedToken,
+            //         tenantId,
+            //         subscriptionId,
+            //         allowNoSubscriptionsLogin,
+            //         environment,
+            //         resourceManagerEndpointUrl);
+            //     await spnlogin.initialize();
+            //     await spnlogin.login();
             // }
             console.log("Login successful.");
         }
